64701d2ced0680051acb2f967c3bc9d959cb8de6
kurs <kurs@DESKTOP-V3HDKPQ>
2023-04-01 14:04:54 +0200

    Add project files.

---
+++ WpfTest/ButtonTest.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for ButtonTest.xaml
+    /// </summary>
+    public partial class ButtonTest : Window
+    {
+        public ButtonTest()
+        {
+            InitializeComponent();
+            btnTest.Content = "ABC";
+            btnTest.Click += ButtonTest_Click;
+        }
+
+        private void ButtonTest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("click!");
+        }
+
+    }
+}
---
+++ WpfTest/WpfResources.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for WpfResources.xaml
+    /// </summary>
+    public partial class WpfResources : Window
+    {
+        public WpfResources()
+        {
+            InitializeComponent();
+        }
+
+        private bool isRed = true;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            string s1 = s.ToUpper();
+
+            var obj = Application.Current.FindResource("DynamicColorBrush");
+            obj = FindResource("text1");
+            obj = stackPanel.FindResource("text1");
+
+            Color color = isRed ? Colors.Green : Colors.Red;
+            SolidColorBrush solid = new SolidColorBrush(color);            
+            Application.Current.Resources["DynamicColorBrush"] = solid;
+            isRed = !isRed;
+        }
+    }
+}
